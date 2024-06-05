836ec7cfdbada107df142d2218208d625f005fda
Abraham Balboa <abrahambalboafuentes74@gmail.com>
2024-06-04 23:20:52 -0500

    version 0.0.1

---
+++ src/main/java/com/saditec/platform/SaditecServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saditec.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaditecServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaditecServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saditec/platform/SaditecServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saditec.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaditecServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
